3952ab5017135106ff9df1a1f0fd61db405aec50
Favour <toluwaniokunowo@gmail.com>
2022-02-11 04:02:00 +0100

    add cards(links) for each available service

---
+++ resources/js/Pages/Choose.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <div class="p-6 sm:px-20 bg-white border-b border-gray-200">
+            <h1 class="text-black font-bold text-4xl">Choose an <span
+                class="underline decoration-double decoration decoration-cyan-500">Option</span></h1>
+        </div>
+
+        <div
+            class="bg-gray-200 bg-opacity-25 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 flex justify-center text-center gap-x-4 gap-y-6 p-6 sm:px-20">
+            <a href="" v-for="card in cards"
+               class="p-6 bg-cyan-300 rounded-lg h-64 flex items-center outline outline-black">
+                <h2 class="text-2xl font-semibold uppercase w-full outline outline-red-600">{{ card.name }}</h2>
+            </a>
+        </div>
+    </div>
+</template>
+
+<script>
+    const cards = [
+        { name: 'Invoice', href: 'something' },
+        { name: 'Receipt', href: 'something' },
+        { name: 'History', href: 'something' },
+    ]
+
+    export default {
+        name: "Choose.vue",
+        setup() {
+            return { cards }
+        }
+    }
+
+</script>
--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -9,7 +9,9 @@
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                    <welcome />
+                    <Choose />
+
+                    <!--                    <welcome />-->
                 </div>
             </div>
         </div>
@@ -20,9 +22,11 @@
     import { defineComponent } from 'vue'
     import AppLayout from '@/Layouts/AppLayout.vue'
     import Welcome from '@/Jetstream/Welcome.vue'
+    import Choose from './Choose.vue'
 
     export default defineComponent({
         components: {
+            Choose,
             AppLayout,
             Welcome,
         },
