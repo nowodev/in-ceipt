cccd329b05f075acad2d0e2b808d1752d968412e
Favour <toluwaniokunowo@gmail.com>
2022-02-24 14:51:19 +0100

    update pdf resolution

--- resources/js/Jetstream/Download.vue
+++ resources/js/Jetstream/Download.vue
@@ -21,6 +21,9 @@
                 let opt = {
                     margin: 1,
                     filename: me.name,
+                    image: { type: 'jpeg', quality: 0.98 },
+                    html2canvas: { scale: 2 },
+                    jsPDF: { format: 'A4', orientation: 'portrait' }
                 };
                 html2pdf().from(invoice).set(opt).save();
             },
