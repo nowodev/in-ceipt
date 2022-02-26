b28f2c9e79f657a988bdf35aab4bd05bbd5edb82
Favour <toluwaniokunowo@gmail.com>
2022-02-26 18:25:14 +0100

    change page title

--- resources/js/Pages/Backend/Customers/Index.vue
+++ resources/js/Pages/Backend/Customers/Index.vue
@@ -1,5 +1,5 @@
 <template>
-    <app-layout title="Invoice">
+    <app-layout title="Customers">
         <template #header>
             <h2 class="flex text-xl font-semibold leading-tight text-gray-800">
                 <Link :href="route('dashboard')">
