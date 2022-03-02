906f0679302af3ee4f7dd3e18fd041102cf3ccf7
Favour <toluwaniokunowo@gmail.com>
2022-03-02 22:27:07 +0100

    create bank & settings migration

---
+++ database/migrations/2022_03_02_211650_create_settings_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('settings', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->string('company_name');
+            $table->string('logo');
+            $table->longText('address');
+            $table->timestamps();
+        });
+
+        Schema::table('settings', function (Blueprint $table) {
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('settings');
+    }
+};
