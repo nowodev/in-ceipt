971b16c2718a859c9d6ae63df7aae6a0378991ee
Favour <toluwaniokunowo@gmail.com>
2022-03-14 16:36:36 +0100

    disable 2fa & account deletion

--- config/jetstream.php
+++ config/jetstream.php
@@ -59,7 +59,7 @@ return [
          Features::profilePhotos(),
         // Features::api(),
         // Features::teams(['invitations' => true]),
-        Features::accountDeletion(),
+//        Features::accountDeletion(),
     ],
 
     /*
