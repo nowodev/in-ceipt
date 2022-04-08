321f2f2da926027bf2edc9262b759fa044d3333b
Favour <toluwaniokunowo@gmail.com>
2022-04-08 18:56:03 +0100

    fill receipt validation request

--- app/Http/Requests/StoreReceiptRequest.php
+++ app/Http/Requests/StoreReceiptRequest.php
@@ -13,7 +13,7 @@ class StoreReceiptRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
@@ -24,7 +24,32 @@ class StoreReceiptRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            'fullname' => ['required', 'string', 'max:255'],
+            'number' => ['sometimes', 'nullable', 'min:11'],
+            'email' => ['nullable', 'email', 'max:255'],
+            'address_1' => ['string', 'sometimes', 'nullable', 'max:255'],
+            'address_2' => ['string', 'sometimes', 'nullable', 'max:255'],
+            'serial_no' => ['required', 'integer', 'digits_between:7,14'], //optional
+            'issue_date' => ['required', 'date'],
+            'payment_method' => ['required'],
+            'info.*.description' => ['required', 'string', 'max:255', 'unique:invoice_details'],
+            'info.*.unit_price' => ['required', 'numeric'],
+            'info.*.quantity' => ['required', 'integer'],
+            'info.*.total' => ['required', 'numeric'],
+            'sum_total' => ['required', 'numeric'],
+            'discount' => ['required', 'integer'],
+            'sub_total' => ['required', 'numeric'],
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'info.*.description.required' => 'The description is required',
+            'info.*.unit_price.required' => 'The unit price is required',
+            'info.*.quantity.required' => 'The quantity is required',
+            'info.*.total.required' => 'The total is required',
+            'info.*.description.unique' => 'This description has already been used',
         ];
     }
 }
